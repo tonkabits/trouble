1a693fb4f0955df89af3f380d3edf2a78a66d1bb
nlgrqve <tonkabits@gmail.com>
2021-07-20 13:23:58 -0400

    added dices, board, player and pieces classes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
+let testGame = {}
+
+
 // window.alert('working JS') //testing JS
+
+// this is a 2 step process, the user press the center button(pop-o-matic)
+// then picks which pieces he wants to move,
+
+// then next player is active
+
+
+
+// this function will require the users to double check they want to refresh
+    // window.onbeforeunload = function (event) {
+    //     return confirm("Confirm refresh");
+    // };
+
+
 
 // this Dice will pick a number between 1 and 6
 class Dice {
@@ -8,6 +25,7 @@ class Dice {
     roll() {
         let result = this.facesArr[Math.floor(Math.random() * this.facesArr.length)]
         console.log(result)
+        return result
     }
 }
 
@@ -19,34 +37,301 @@ class SpecialDice {
     roll() {
         let result = this.facesArr[Math.floor(Math.random() * this.facesArr.length)]
         console.log(result)
+        return result
     }
 }
 
 // user throw dices 
 function throwDices() {
     let dice1 = new Dice
-    dice1.roll()
+    dice1 = dice1.roll()
+
     let dice2 = new SpecialDice
-    dice2.roll()
+    dice2 = dice2.roll()
+
+
+    let dice1dom = document.getElementById('dice1dom')
+    let dice2dom = document.getElementById('dice2dom')
+    dice1dom.innerHTML = dice1
+    dice2dom.innerHTML = dice2
+    testGame.dice1 = dice1
+    testGame.dice2 = dice2
+    return (dice1, dice2)
 }
 
 // user see results of the dice throw and pick which piece to move
 
 
 
+function addDices() {
+    if(typeof(testGame.dice2) === 'number'){
+        console.log('dices are numbers')
+        let sum = testGame.dice1 + testGame.dice2
+        testGame.red.lastThrow = sum
 
-throwDices()
-
-// movePiece()
-function movePiece(player, piece){
-// if player is active
-//  move piece
+    }else if(testGame.dice2 === 'Miss Fritter'){
+        console.log(`we have ${testGame.dice1} plus ${testGame.dice2}`)
+        testGame.red.lastThrow = testGame.dice1
+        testGame.red.hasMissFritter = true
+    } else if (testGame.dice2 === 'Roadblock'){
+        console.log('roadblock')
+        if(testGame.red.hasMissFritter){
+            testGame.red.lastThrow = testGame.dice1
+        }else{
+            testGame.currentTurn = 'blue'
+            console.log('you miss a turn')
+            // i need to make a cycle logic to acomodate for colors schedule
+            // goToNextPlayer()
+        }
+    }
 }
 
-// bluePlayer.piece1.makeMove()
 
-// newGame(numerOfPlayers)
 
-// class Player(color, name){}
-//   
-// 
+
+class Player {
+    constructor(color, name) {
+        this.color = color,
+        this.name = name,
+        this.hasMissFritter = false,
+        this.lastThrow = 0,
+        this.finishedPieces = 0
+    }
+    make(){
+        // if this color === red 
+        switch(this.color){
+            case 'red' :
+                let redPlayer = {
+                    color: this.color,
+                    name: this.name,
+                    hasMissFritter: this.hasMissFritter,
+                    lastThrow: this.lastThrow,
+                    finishedPieces: this.finishedPieces
+                } 
+                let playerRedPiece = []
+                let startPositionRed = 1
+                for(let i = 0; i < 4; i++){
+                    playerRedPiece[i] = new Piece(this.color, startPositionRed)
+                    // console.log(`This is piece-${i}, of ${this.color} color, and they start at position 1`)
+                }
+                redPlayer.pieces = playerRedPiece
+                return redPlayer
+            break
+
+            // object mode does not work yet
+            case 'blue' :
+                let playerBluePiece = {}
+                let startPositionBlue = 9
+                for (let i = 1; i <= 4; i++) {
+                    playerBluePiece.piece[i] =  new Piece(this.color, startPositionBlue) 
+                    // console.log(`This is piece-${i}, of ${this.color} color, and they start at position 9`)
+                   
+                }
+                return playerBluePiece
+            break
+        }
+    } 
+}
+
+class Piece {
+    constructor(color, startPosition){
+        this.color = color,
+        this.startPosition = startPosition,
+        this.currentPosition = startPosition
+        
+    }
+
+}
+
+// let testPlayer = new Player('blue', 'testName')
+// let testMake = new Player('red', 'Rayo')
+// let testGenerate = new Player('red', 'Rojito') 
+// testGenerate = testGenerate.make()
+// console.log(testGenerate)
+
+
+// let testGenerate2 = new Player('blue', 'Azulito') 
+// testGenerate2 = testGenerate2.make()
+// console.log({testGenerate2})
+//  console.log(testPlayer)
+
+
+let boardGame = {
+    currentTurn: 'blue',
+    1: 'empty',
+    2: 'empty',
+    3: 'empty',
+    4: 'empty',
+    5: 'empty',
+    6: 'empty'
+}
+
+// if current turn blue make pop-O-matic blue
+// throw dices
+// pick piece
+// make move
+// update current turn
+
+
+class Game {
+    constructor(numberOfPlayers){
+        this.numberOfPlayers = numberOfPlayers
+
+    }
+
+    makeBoard(){
+        let boardGame = {}
+        boardGame.currentTurn = 'red'
+        boardGame.blocks = []
+        for(let i = 1; i <= 32; i++){
+            boardGame.blocks[i] = 'emptyss'
+        }
+        boardGame.blocks.shift() //remove first item of the array to make just 32 
+        return boardGame
+    }
+
+    start() {
+        for(let i = 0; i < this.numberOfPlayers; i++){
+          let Game = new Player(color, name)
+        }
+    }
+
+    makePlayer() {
+
+    }
+
+    hasWinner(){
+        // if any player has four pieces in the finish line make him the Winner
+    }
+
+
+
+}
+
+
+
+// function drawPiecesThatFinished(testGame){
+    
+//     let redFinishLine = document.getElementById('red-finish-line')
+//     testGame.red.pieces.forEach(piece = () => {
+//         if(piece.crossedFinishLine){
+//             let finishedPiece = document.createElement('div').classList.add('class="h-6 w-6 bg-red-700 rounded-full flex items-center justify-center')
+//             return redFinishLine += finishedPiece
+//         }
+//     })
+// }
+
+// movePiece()
+function movePiece(id){
+    // alert(`piece is moving ${id}`)
+    // console.log(`test generate inside ${redP}`)
+
+        console.log(`test game inside ${testGame.boardGame}`)
+        let btn = document.getElementById(`${id}`)
+        btn.remove()
+        let index = id.slice(-1);// pick the number portion of the id of the dom element we are targeting
+        let prev = testGame.red.pieces[index].currentPosition
+        let i = testGame.red.lastThrow + prev
+        if(i < 32){
+            let destination = document.getElementById(`${i}`)
+            let newBtn = `<button id="${id}"class="h-12 w-12 bg-red-500 rounded-full flex items-center justify-center text-white" onclick='movePiece(this.id)'></button>`
+            destination.innerHTML = newBtn
+        }else{
+            console.log('crossed the line')
+            return  testGame.red.finishedPieces += 1
+        }
+        return testGame.red.pieces[index].currentPosition = i
+
+    
+
+
+}
+
+let players = document.getElementById('startgame')
+players.addEventListener('click', () => {
+
+    // we initialize the game board
+    testGame = new Game
+    testGame = testGame.makeBoard()
+    console.log('start clicked')
+    console.log({ testGame })
+    // we initialize the active players
+    let redPlayerName = document.getElementById('redplayer')
+    if(redPlayerName && redPlayerName.value){
+        let redP = new Player('red', redPlayerName.value )
+        redP = redP.make()
+        testGame.red = redP
+        console.log({redP})
+        console.log(redP.name)
+
+        let redPiecesHtml = `<div class="flex flex-col">
+                    <div class="text-xl text-center">
+                        ${redP.name} - Team McQueen
+                    </div>
+                    <div class="flex">
+                        <button id="red-piece-1"class="h-12 w-12 bg-red-500 rounded-full flex items-center justify-center text-white" onclick='movePiece(this.id)'>1</button>
+                        <button id="red-piece-2"class="h-12 w-12 bg-red-500 rounded-full flex items-center justify-center text-white" onclick='movePiece(this.id)'>2</button>
+                        <button id="red-piece-3"class="h-12 w-12 bg-red-500 rounded-full flex items-center justify-center text-white" onclick='movePiece(this.id)'>3</button>
+                        <button id="red-piece-4"class="h-12 w-12 bg-red-500 rounded-full flex items-center justify-center text-white" onclick='movePiece(this.id)'>4</button>
+                    </div>
+                </div>`
+
+        let redPits = document.getElementById('red-pits')
+        redPits.innerHTML = redPiecesHtml
+        // console.log(redP.color)
+        return redP
+    }else{
+        console.log('this is null')
+    }
+
+    let bluePlayerName = document.getElementById('blueplayer').value
+    let yellowPlayerName = document.getElementById('yellowplayer').value
+    let greenPlayerName = document.getElementById('greenplayer').value
+    console.log(redPlayerName, bluePlayerName, yellowPlayerName, greenPlayerName)
+    
+    return testGame
+})
+
+
+let pop = document.getElementById('pop-o-matic')
+pop.addEventListener('click', () =>{
+    throwDices()
+    addDices()
+
+    switch(testGame.currentTurn){
+        case 'red':
+            console.log('do red turn logic')
+            testGame.currentTurn = 'blue'
+            pop.classList.remove('bg-red-500')
+            pop.classList.add('bg-' + testGame.currentTurn + '-500')
+
+        break
+        case 'blue':
+       
+            console.log('do blue turn logic')
+            testGame.currentTurn = 'yellow'
+            pop.classList.remove('bg-blue-500')
+            pop.classList.add('bg-' + testGame.currentTurn + '-500')
+        break
+        case 'yellow':
+      
+            console.log('do yellow turn logic')
+            testGame.currentTurn = 'green'
+            pop.classList.remove('bg-yellow-500')
+            pop.classList.add('bg-' + testGame.currentTurn + '-500')
+            break
+        case 'green':
+      
+            console.log('do green turn logic')
+            testGame.currentTurn = 'red'
+            pop.classList.remove('bg-green-500')
+            pop.classList.add('bg-' + testGame.currentTurn + '-500')
+        break
+    }
+})
+
+
+// testGame.currentTurn = 'blue'
+
+
+// console.log({testGame})
